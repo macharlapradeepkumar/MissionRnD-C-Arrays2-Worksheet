83323e3d89be6e4a287a99a635abb814de0c5f1e
macharlapradeepkumar <macharlapradeepkumar@missionrnd.com>
2016-03-18 22:35:15 +0530

    Submit Commit

--- src/countGreaterNumbers.cpp
+++ src/countGreaterNumbers.cpp
@@ -19,7 +19,35 @@ struct transaction {
 	char date[11];
 	char description[20];
 };
-
+int num(char *dob1,int i);
 int countGreaterNumbers(struct transaction *Arr, int len, char *date) {
-	return -1;
+	int oneday = 0, onemon = 0, oneyar = 0, secday = 0, secmon = 0, secyar = 0, l=0,m=0;
+	oneday = num(date ,0);  
+	onemon = num(date, 3);
+	oneyar = num(date, 6);
+	for (int i = 0; i < len; i++){
+		secday = num(Arr[i].date, 0);
+		secmon = num(Arr[i].date, 3);
+		secyar = num(Arr[i].date, 6);
+		if ((oneyar == secyar) && (onemon == secmon) && (oneday == secday))
+			l++;
+		if (l > 0)
+		{
+			if ((oneyar != secyar) || (onemon != secmon) || (oneday != secday))
+				m++;
+		}
+	}
+	return m;
+}
+int num(char *a, int i)
+{
+	int  k = 0;
+	while (a[i] != '-' && a[i] != '\0')
+	{
+		if (a[i] >= 48 || a[i] > 58)
+
+			k = k * 10 + (a[i] - '0');
+       	i++;
+	}
+	return k;
 }
